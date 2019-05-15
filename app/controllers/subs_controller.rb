ac136972844be5a65396af4fadcccd59f21dbd08
agarcian031 <algarcian@outlook.com>
2019-05-15 11:19:58 -0600

    CRUD for subs and partials

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,18 +1,20 @@
 class SubsController < ApplicationController
+  # before_action :set_sub, except: [:index, :new, :create]
+  before_action :set_sub, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @subs = Sub.all 
     # render :index 
   end
 
   def show
-    # url = /subs/2(:id)
-    #params = {id: 2}
-    @sub = Sub.find(params[:id])
     #render :show 
   end
 
   def new
-    @sub = Sub.new 
+    @sub = Sub.new
+    render partial: "form" 
     # render: new 
   end
 
@@ -31,11 +33,10 @@ class SubsController < ApplicationController
 
   def edit
     # render :edit 
-    @sub = Sub.find(params[:id])
+    render partial: "form" 
   end
 
   def update 
-    @sub = Sub.find(params[:id])
     if @sub.update(sub_params)
       redirect_to sub_path(@sub.id)
     else
@@ -44,7 +45,6 @@ class SubsController < ApplicationController
   end 
 
   def destroy 
-    @sub = Sub.find(params[:id])
     @sub.destroy 
     redirect_to subs_path
   end 
@@ -55,5 +55,11 @@ class SubsController < ApplicationController
   def sub_params
     # params = {sub: { name: "Sub Name"}}
     params.require(:sub).permit(:name)
+  end 
+
+  def set_sub
+    # url = /subs/2(:id)
+    #params = {id: 2}
+    @sub = Sub.find(params[:id])
   end 
 end
